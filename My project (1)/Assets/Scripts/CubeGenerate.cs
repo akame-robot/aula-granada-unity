c3272f21bf448898abec2a6461dd368bf7649ee4
akame-robot <wolfinfiresnow@gmail.com>
2023-05-22 21:52:31 -0400

    commit

--- My project (1)/Assets/Scripts/CubeGenerate.cs
+++ My project (1)/Assets/Scripts/CubeGenerate.cs
@@ -20,17 +20,15 @@ public class CubeGenerate : MonoBehaviour
         {
             for (int vertic = 0; vertic < boardSize; vertic++)
             {
+
                 // Instancia um cubo usando o prefab fornecido, definindo sua posição e rotação
-                GameObject tile = Instantiate(boardPrefab, new Vector3(horiz, 0, vertic), Quaternion.identity);
+                GameObject tile = Instantiate(boardPrefab, new Vector3(horiz, vertic, 0), Quaternion.identity);
+
                 // Define o objeto pai do cubo como o transform atual (provavelmente o objeto que possui esse script)
                 tile.transform.SetParent(transform);
 
-                // Verifica se a célula está em uma linha horizontal par ou ímpar e em uma coluna vertical par ou ímpar
-                bool isOnHoriz = horiz % 2 == 0;
-                bool isOnVertic = vertic % 2 == 0;
-
                 // Define a cor do material do cubo com base na posição da célula
-                if ((isOnHoriz && isOnVertic) || (!isOnHoriz && !isOnVertic))
+                if ((horiz + vertic) % 2 == 0)
                 {
                     tile.GetComponent<Renderer>().material.color = Color.white;
                 }
