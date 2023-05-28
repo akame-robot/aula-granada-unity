dfd07bc4b5b035f0409f1351fe30d880094c58ac
akame-robot <wolfinfiresnow@gmail.com>
2023-05-27 22:54:48 -0400

    more upgrade to de game

--- My project (1)/Assets/Scripts/FlappyBird.cs
+++ My project (1)/Assets/Scripts/FlappyBird.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,21 +10,42 @@ public class FlappyBird : MonoBehaviour
 
     public float jumpForce = 10f;
     public float gravityForce = 10f;
+    public bool isPaused = false;
+    public bool isDead = false;
+    public bool canControll = true;
 
     private Rigidbody rb;
+    private BoxCollider overtake;
 
     void Start()
     {
+        canControll = true;
         rb = GetComponent<Rigidbody>();
+        overtake = GetComponent<BoxCollider>();
+        Time.timeScale = 0f;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButton(0)) 
+        if (Input.GetMouseButtonDown(0))
         {
-            rb.velocity = new Vector3(0, jumpForce, 0);
+            Time.timeScale = 1f;
         }
-        rb.AddForce(new Vector3(0, -gravityForce, 0));
+        if (canControll && !isDead)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                rb.velocity = new Vector3(0, jumpForce, 0);
+            }
+            rb.AddForce(new Vector3(0, -gravityForce, 0));
+        }
+        else if (isDead)
+        {
+            Vector3 newPos = new Vector3(transform.position.x, transform.position.y, 12f);
+            Debug.Log("entered here");
+            canControll = false;
+            transform.position = newPos;
+        }
 
         CharacterDie();
 
@@ -30,7 +53,7 @@ public class FlappyBird : MonoBehaviour
 
     void CharacterDie()
     {
-        if (transform.position.y >= 5.48f || transform.position.y <= -5.48f)
+        if (transform.position.y >= 6.48f || transform.position.y <= -6.48f)
         {
             Destroy(gameObject);
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
@@ -41,10 +64,8 @@ public class FlappyBird : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Pipe"))
         {
-            Destroy(gameObject);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            Debug.Log("colliding");
+            isDead = true;
         }
     }
-
-
 }
--- My project (1)/Assets/Scripts/Obstacle.cs
+++ My project (1)/Assets/Scripts/Obstacle.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour
 {
 
+    private FlappyBird dead;
     public float velocity = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-velocity * Time.deltaTime,0,0);
+
+        GameObject scriptWithBool = GameObject.Find("Cube");
+        dead = scriptWithBool.GetComponent<FlappyBird>();
+        bool died = dead.isDead;
+
+        transform.Translate(-velocity * Time.deltaTime, 0, 0);
         if (transform.position.x <= -30f)
         {
             Destroy(gameObject);
+        }
+
+        else if (died)
+        {
+            velocity = 0f;
         }
     }
 }
